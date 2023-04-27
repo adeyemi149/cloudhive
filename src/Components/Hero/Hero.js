746b32835d7bb69056119104457b68297ffcf1f1
Alimi Adeyemi <adeyemi149@gmail.com>
2023-04-27 15:47:51 +0100

    change hero section

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -3,32 +3,21 @@ import bgImg from "../../assests/cyber-bg.png"
 import { CloudArrowUpIcon, CircleStackIcon, PaperAirplaneIcon, ServerStackIcon, } from '@heroicons/react/24/solid'
 
 const Hero = () => {
-  return (
-	  <div name="home" className='w-full h-screen bg-zinc-200 flex flex-col justify-between md:mt-11'>
-		  <div className='grid md:grid-cols-2 max-w-[1240px] m-auto justify-center '>
-			  <div className='flex flex-col justify-center md:items-start w-full px-2 py-4'>
-				  <p className='text-2xl'>Unique Sequencing & <span className=''>Production</span></p>
-				  <h1 className='py-3 text-5xl md:text-7xl font-bold'>Cloud Management</h1>
-				  <p className='text-2xl'>This is our Tech brand.</p>
-				  <button className='px-6 py-3 bg-indigo-500 rounded-lg sm:w-[60%] my-4 text-white'>Get Started</button>
-			  </div>
-			  <div className='flex justify-center'>
-				  <img className='w-3/4 md:w-full md:animate-bounce' src={bgImg} alt="/"/>
-			  </div>
-			  <div className='absolute flex flex-col justify-center items-center py-8 w-3/4 md:min-w-[760px] bottom-[-7%] md:bottom-[-15%] 
-			  mx-1 mt-6 md:left-1/2 transform md:-translate-x-1/2 bg-zinc-200 border 
-			  border-slate-300 rounded-xl text-center shadow-xl'>
-				  <p>Data Services</p>
-				  <div className='flex justify-center md:justify-between items-start flex-wrap px-4'>
-					  <p className='flex px-4 py-2 text-slate-500'><CloudArrowUpIcon className='h-6 text-indigo-600'/> App Security</p>
-					  <p className='flex px-4 py-2 text-slate-500'><CircleStackIcon className='h-6 text-indigo-600'/> Dashboard Design</p>
-					  <p className='flex px-4 py-2 text-slate-500'><ServerStackIcon className='h-6 text-indigo-600'/> Cloud Data</p>
-					  <p className='flex px-4 py-2 text-slate-500'><PaperAirplaneIcon className='h-6 text-indigo-600'/> API</p>
-				  </div>
-			  </div>
-		  </div>
-	</div>
-  )
+	return (
+		<div name="home" className='w-full bg-zinc-200 flex flex-col justify-between md:mt-11'>
+			<div className='grid md:grid-cols-2 max-w-[1240px] mx-auto mt-10 justify-center '>
+				<div className='flex flex-col justify-center md:items-start w-full px-2 pt-20'>
+					<p className='text-2xl'>Unique Sequencing & <span className=''>Production</span></p>
+					<h1 className='py-3 text-5xl md:text-7xl font-bold'>Cloud Management</h1>
+					<p className='text-2xl'>This is our Tech brand.</p>
+					<button className='px-6 py-3 bg-indigo-500 rounded-lg below:w-full sm:w-[60%] my-4 text-white'>Get Started</button>
+				</div>
+				<div className='flex justify-center below:pb-2'>
+					<img className='w-full' src={bgImg} alt="/" />
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Hero
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,14 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
-  //Everything below @media (min-width: 640px)
   theme: {
     screens: {
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
+      "below-400": {"max": "400px"},
+      "below": {"max": "640px"},
+      "sm": "640px",
+      "md": "768px",
+      "lg": "1024px",
+      "xl": "1280px",
       "2xl": "1536px"
   },
     extend: {
